739eb213479b15a04ab0049a2b445c67da6ae672
psovit <sovit.poudel@gmail.com>
2019-08-23 14:01:34 +0545

    code_highlighter: added method to update styles remotely, get span for position & code clean up

--- lib/code_editor/code_highlighter.dart
+++ lib/code_editor/code_highlighter.dart
@@ -5,20 +5,73 @@ import 'package:rich_code_editor/code_editor/widgets/code_editing_value.dart';
 
 // This is the interface that must be implemented by Syntax highlighter's for each programming language
 abstract class CodeEditingValueHighlighterBase {
+
+  /// This is the main method that must be implemented by each highlighter.
   CodeEditingValue parse(
       {@required CodeEditingValue oldValue,
       @required CodeEditingValue newValue,
       @required TextStyle style});
 
-  /// Returns the [TextSpan] along with it's [index] in the spans list 
+  /// Returns the [TextSpan] along with it's [index] in the spans list
   /// that contains the given position in the text.
-  /// Pass [oldValue.value] & [oldValue.selection.baseOffset] as params here.
-  Map<int, TextSpan> getSpanForPosition(TextSpan parent, int targetOffset);
+  /// This method gets called from [CodeEditableText] during reset selection change event.
+  /// It can be used as required during parse as well.
+  @override
+  Map<int, TextSpan> getSpanForPosition(TextSpan parent, int targetOffset) {
+    var map = Map<int, TextSpan>();
+    int offset = 0;
+    if (parent.children == null) {
+      map[0] = parent;
+      return map;
+    }
+
+    var i = 0;
+    parent.children.forEach((child) {
+      var tspan = child as TextSpan;
+
+      if (tspan.children != null && tspan.children.length > 0) {
+        //check in children
+        bool foundInChildren = false;
+
+        tspan.children.forEach((child2) {
+          var tspan2 = child2 as TextSpan;
+          var endOffset = offset + tspan2.text.length;
+          if (targetOffset >= offset && targetOffset <= endOffset) {
+            map[i] = tspan2;
+            foundInChildren = true;
+            return;
+          }
+          offset = endOffset;
+        });
+
+        if (foundInChildren) return;
+      } else {
+        var endOffset = offset + tspan.text.length;
+        if (targetOffset >= offset && targetOffset <= endOffset) {
+          map[i] = tspan;
+          return;
+        }
+        offset = endOffset;
+      }
+
+      i++;
+    });
+    return map;
+  }
+
+  /// Insert text programatically/remotely. Method should return new CodeEditingValue with inserted text.
+  CodeEditingValue addTextRemotely(CodeEditingValue oldValue, String newText);
+
+  /// Re-generate styles for the provided code editing value.
+  CodeEditingValue updateStyle(CodeEditingValue value) {
+    //implement as required in base class..
+    return value;
+  }
 }
 
 // This is a dummy implementation of syntax highlighter
 // Highlights a different color for every other word.
-class DummyHighlighter implements CodeEditingValueHighlighterBase {
+class DummyHighlighter extends CodeEditingValueHighlighterBase {
   var _totalOffset = 0;
   var kCodeStyle = TextStyle(fontSize: 16.0, color: Colors.green);
   var plainStyle = TextStyle(fontSize: 16.0, color: Colors.black);
@@ -39,10 +92,9 @@ class DummyHighlighter implements CodeEditingValueHighlighterBase {
     final TextSelection newSelection = newValue.selection;
 
     if (_enterPressed(oldValue, newValue)) {
-      print('enter pressed');
-
       return addTextRemotely(newValue, "    ");
-    } 
+    }
+
     var ls = _getTextSpans(newValue.text);
 
     if (ls.length > 1) {
@@ -57,14 +109,13 @@ class DummyHighlighter implements CodeEditingValueHighlighterBase {
     return newValue;
   }
 
-  static bool _equalTextValue(CodeEditingValue a, CodeEditingValue b) {
+  bool _equalTextValue(CodeEditingValue a, CodeEditingValue b) {
     return a.value.toPlainText() == b.value.toPlainText() &&
         a.selection == b.selection &&
         a.composing == b.composing;
   }
 
-  bool _enterPressed(
-      CodeEditingValue oldValue, CodeEditingValue newValue) {
+  bool _enterPressed(CodeEditingValue oldValue, CodeEditingValue newValue) {
     if (newValue.text.length == 1) {
       return (newValue.text == "\n");
     }
@@ -79,14 +130,6 @@ class DummyHighlighter implements CodeEditingValueHighlighterBase {
     var lastChar = newValue.text
         .substring(currentSelection.baseOffset, newSelection.baseOffset);
     return (lastChar == "\n");
-  }
-
-  bool _backSpacePressed(
-      CodeEditingValue oldValue, CodeEditingValue newValue) {
-    final TextSelection newSelection = newValue.selection;
-    final TextSelection currentSelection = oldValue.selection;
-
-    return currentSelection.baseOffset > newSelection.baseOffset;
   }
 
   bool _sameTextDiffSelection(CodeEditingValue a, CodeEditingValue b) {
@@ -108,23 +151,27 @@ class DummyHighlighter implements CodeEditingValueHighlighterBase {
 
     var result = "";
 
-    if (textBefore.length > 0) {
-      result = textBefore + newText;
-    }
+    if (textBefore.length == 0) {
+      // at the beginning of editor
+      result = newText;
+    } else {
+      if (textBefore.length > 0) {
+        result = textBefore + newText;
+      }
 
-    if (textAfter.length > 0) {
-      result = result + textAfter;
+      if (textAfter.length > 0) {
+        result = result + textAfter;
+      }
     }
 
     var ls = _getTextSpans(result);
     oldValue = oldValue.copyWith(
-      remotelyEdited: true,
-        value: new TextSpan(
-            text: "", style: plainStyle, children: ls),
+        remotelyEdited: true,
+        value: new TextSpan(text: "", style: plainStyle, children: ls),
         selection: TextSelection.fromPosition(TextPosition(
             affinity: TextAffinity.downstream,
             offset: currentSelection.start + newText.length)));
-    
+
     return oldValue;
   }
 
@@ -136,7 +183,7 @@ class DummyHighlighter implements CodeEditingValueHighlighterBase {
     var lineSpan = TextSpan(text: "\n", style: plainStyle);
     for (var i = 0; i < lines.length; i++) {
       var line = lines[i];
-      if(line == "    ") {
+      if (line == "    ") {
         ls.add(TextSpan(text: "    ", style: plainStyle));
         continue;
       }
@@ -166,8 +213,18 @@ class DummyHighlighter implements CodeEditingValueHighlighterBase {
   }
 
   @override
-  Map<int, TextSpan> getSpanForPosition(TextSpan parent, int targetOffset) {
-    // TODO: implement getSpanForPosition
-    return null;
+  CodeEditingValue updateStyle(CodeEditingValue value) {
+    var ls = _getTextSpans(value.text);
+
+    if (ls.length > 1) {
+      var newValue = value.copyWith(
+          value: new TextSpan(text: "", style: plainStyle, children: ls),
+          selection: value.selection,
+          remotelyEdited: true);
+      _totalOffset = 0;
+      return newValue;
+    }
+
+    return value;
   }
 }
